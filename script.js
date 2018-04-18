189258969fb2b68cd0c8ec9d229044b122aee8e0
Iona Sammons <ionakat@gmail.com>
2018-04-19 11:05:30 +1200

    make contactSubmit use validateForm function

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-function formSubmitted () {
-	alert("Thank you for your message! Someone will be in contact within the next couple days.");
+
+//contactSubmitted and subscribeSubmitted both check if all the feilds have been filled using validateForm
+//if all feilds have been filled then the approriate message is displayed
+
+function contactSubmitted (formname, formfeild1, formfeild2, formfeild3) {
+	var status = validateForm (formname, formfeild1, formfeild2, formfeild3)
+	var status = validateForm (formname, formfeild1, formfeild2, formfeild3)
+	if (status == true) {
+		document.getElementById("contactform").innerHTML = "Thank you for your message! Someone will be in contact in a few days.";		
+	}
 }
 
-
-
-
-
-function subscribeSubmitted () {
-	var status = validateForm ()
+function subscribeSubmitted (formname, formfeild1, formfeild2, formfeild3) {
+	var status = validateForm (formname, formfeild1, formfeild2, formfeild3)
 	if (status == true) {
 		document.getElementById("subscribeform").innerHTML = "Thank you for subscribing! You'll get an email soon to verify your details.";		
 	}
 }
 
-function validateForm () {
-    var a=document.forms["subscribe"]["name"].value;
-    var b=document.forms["subscribe"]["email"].value;
-    var c=document.forms["subscribe"]["password"].value;
+function validateForm (formname, formfeild1, formfeild2, formfeild3) {
+    var a=document.forms[formname][formfeild1].value;
+    var b=document.forms[formname][formfeild2].value;
+    var c=document.forms[formname][formfeild2].value;
     if (a==null || a=="",b==null || b=="",c==null || c=="") {
         alert("Please fill in all the fields");
         return false;
